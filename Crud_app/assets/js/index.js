8948ed16e322151e8d80de8fdad4c4a437ec7b57
Yashavant Singh <singhyashavant2001@gmail.com>
2022-09-26 23:13:58 +0530

    Crud Project

---
+++ Crud_app/assets/js/index.js
@@ -0,0 +1,53 @@
+
+
+
+$("#add_user").submit(function(event){
+  alert("Data Inserted Successfully!");
+})
+
+$("#update_user").submit(function(event){
+   event.preventDefault();
+
+   var unindexed_array= $(this).serializeArray();
+   var data= {}
+
+   $.map(unindexed_array,function(n,i){
+     data[n['name']]= n['value']
+   })
+
+   console.log(unindexed_array);
+
+   var request= {
+     "url" : `http://localhost:3005/api/users/${data.id}`,
+     "method" : "PUT",
+     "data" : data
+   }
+ 
+   $.ajax(request).done(function(response){
+     alert("Data Updated Successfully!");
+   })
+
+
+})
+
+if(window.location.pathname== "/"){
+   $ondelete= $(".table tbody td a.delete");
+   $ondelete.click(function(){
+    var id = $(this).attr("data-id")
+
+    var request= {
+      "url" : `http://localhost:3005/api/users/${id}`,
+      "method" : "DELETE",
+    }
+
+    if(confirm("Do you really want to delete this record?")){
+      $.ajax(request).done(function(response){
+        alert("Data Deleted Successfully!");
+        location.reload()
+
+      })
+    }
+    
+
+   })
+}
